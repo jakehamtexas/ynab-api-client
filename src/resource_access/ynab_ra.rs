ae75da8456aa9b8c592afc045f389fb312b4551c
Jake Hamilton <jakehamtexas@gmail.com>
2021-01-09 00:04:12 -0600

    Do not use reqwest::Result

--- src/resource_access/ynab_ra.rs
+++ src/resource_access/ynab_ra.rs
@@ -1,3 +1,4 @@
+use crate::failure::Result;
 use crate::types::{
     account::{Accounts, AccountsResponse},
     budget::{Budgets, BudgetsResponse},
@@ -15,25 +16,25 @@ fn client() -> Client {
     )
 }
 
-pub async fn accounts(budget_id: &str, token: &str) -> reqwest::Result<Accounts> {
+pub async fn accounts(budget_id: &str, token: &str) -> Result<Accounts> {
     let endpoint = get_endpoint("/budgets/{budget_id}/accounts", Some(budget_id));
     let response = get::<AccountsResponse>(&endpoint, token).await?;
     Ok(response.data)
 }
 
-pub async fn budgets(token: &str) -> reqwest::Result<Budgets> {
+pub async fn budgets(token: &str) -> Result<Budgets> {
     let endpoint = get_endpoint("/budgets", None);
     let response = get::<BudgetsResponse>(&endpoint, token).await?;
     Ok(response.data)
 }
 
-pub async fn categories(budget_id: &str, token: &str) -> reqwest::Result<Categories> {
+pub async fn categories(budget_id: &str, token: &str) -> Result<Categories> {
     let endpoint = get_endpoint("/budgets/{budget_id}/categories", Some(budget_id));
     let response = get::<CategoriesResponse>(&endpoint, token).await?;
     Ok(response.data)
 }
 
-pub async fn payees(budget_id: &str, token: &str) -> reqwest::Result<Payees> {
+pub async fn payees(budget_id: &str, token: &str) -> Result<Payees> {
     let endpoint = get_endpoint("/budgets/{budget_id}/payees", Some(budget_id));
     let response = get::<PayeesResponse>(&endpoint, token).await?;
     Ok(response.data)
@@ -43,32 +44,35 @@ pub async fn add_transaction(
     request: &TransactionRequest,
     budget_id: &str,
     token: &str,
-) -> reqwest::Result<StatusCode> {
+) -> Result<StatusCode> {
     let endpoint = get_endpoint("/budgets/{budget_id}/transactions", Some(budget_id));
     let response = post(request, &endpoint, token).await?;
     Ok(response.status())
 }
 
-async fn get<U: DeserializeOwned>(endpoint: &str, token: &str) -> reqwest::Result<U> {
+async fn get<U: DeserializeOwned>(endpoint: &str, token: &str) -> Result<U> {
     let client = client();
     let url = get_url(endpoint);
-    client
+    let u = client
         .get(url)
         .bearer_auth(token)
         .send()
         .await?
         .json::<U>()
-        .await
+        .await?;
+    Ok(u)
 }
 
-async fn post<T: Serialize>(
-    body: &T,
-    endpoint: &str,
-    token: &str,
-) -> reqwest::Result<reqwest::Response> {
+async fn post<T: Serialize>(body: &T, endpoint: &str, token: &str) -> Result<reqwest::Response> {
     let client = client();
     let url = get_url(endpoint);
-    client.post(url).bearer_auth(token).json(&body).send().await
+    let response = client
+        .post(url)
+        .bearer_auth(token)
+        .json(&body)
+        .send()
+        .await?;
+    Ok(response)
 }
 
 fn get_endpoint(endpoint_base: &str, budget_id: Option<&str>) -> String {
